08c166e44b9dfcd7f8fcfeed6b03d2317327be69
Yimin Arava <85930454+YiminArava4508@users.noreply.github.com>
2021-08-12 00:38:23 -0700

    Update app.R
    Added  2 dynamic plots with their own variable selection panel

--- app.R
+++ app.R
@@ -7,40 +7,68 @@ require(scales)
 water_potability <- read.csv(file = 'water_potability.csv')
 # Create user interface --------------------------------------------------------
 ui <- dashboardPage(
-    dashboardHeader(title = "Project1"),
-    dashboardSidebar(
-      sidebarMenu(
-        # Tab 1 -- Dashboard
-        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-        # Tab 2 -- Raw data 
-        menuItem("Raw data", tabName = "rawdata", icon = icon("table"))
-      )
-    ),
-    dashboardBody(
-      tabItems(
-        # Tab 1 body -- Dashboard
-        tabItem(tabName = "dashboard",
-          fluidRow(
-            box(plotOutput("histogram", height = 300)),
-            
-            box(
-              title = "Controls",
-              sliderInput("slider", "Number of observations:", 1, 3276, 1500),
-              sliderInput("slider_bins","Number of bins:",1,60,30)
-            )
-            )
-          ),
-        # Tab 2 body -- Raw data
-        tabItem("rawdata",
-                downloadButton("downloadCsv", "Download as CSV"),
-                tableOutput("contents")
-        )
-        )
+  dashboardHeader(title = "Shiny Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      # Tab 1 -- Dashboard -----------------------------------------------------
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      # Tab 2 -- Raw data ------------------------------------------------------
+      menuItem("Raw data", tabName = "rawdata", icon = icon("table"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # Tab 1 body -- Dashboard ------------------------------------------------
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                # Histogram graph ----------------------------------------------
+                box(plotOutput("histogram", height = 240)),
+                # Slider for Histogram -----------------------------------------
+                box(
+                  title = "Slider for Histogram",
+                  sliderInput("slider", "Number of observations:",
+                              1, 3276, 1500),
+                  sliderInput("slider_bins","Number of bins:",1,60,30)
+                )
+              ),
+              fluidRow(
+                # Scatter plot graph -------------------------------------------
+                box(plotOutput("scatterplot")),
+                # Scatter plot input variables ---------------------------------
+                box(
+                  title = "Select variables to display",
+                  varSelectInput("variable_for_x", "Variable for X:", 
+                                 water_potability),
+                  varSelectInput("variable_for_y", "Variable for Y:",
+                                 water_potability, selected = "Hardness")
+                )
+              ),
+              fluidRow(
+                # Dot plot graph -----------------------------------------------
+                box(
+                  title = "Variable VS Potability",
+                  plotOutput("dotplot")
+                ),
+                # Dot plot input variables -------------------------------------
+                box(
+                  title = "Select a Variable",
+                  varSelectInput("variable_potability", "Variable for Y",
+                                 water_potability)
+                )
+              )# end of fluid row
+      ),
+      # Tab 2 body -- Raw data -------------------------------------------------
+      tabItem("rawdata",
+              downloadButton("downloadCsv", "Download as CSV"),
+              numericInput("maxrows", "Rows to show", 25),
+              verbatimTextOutput("contents")
       )
     )
+  )
+)
 # Server logistics--------------------------------------------------------------
 server <- function(input, output) {
-  # Render a Histogram in the Dashboard tab --------------------------
+  # Render a Histogram in the Dashboard tab ------------------------------------
   output$histogram <- renderPlot({
     WaterPH <- as.data.frame(water_potability$ph[1:input$slider])
     ggplot(data = WaterPH) + aes(x = WaterPH[,1], y = ..density..) + 
@@ -48,14 +76,31 @@ server <- function(input, output) {
       scale_x_continuous("pH", c(0:14)) +
       scale_y_continuous(labels = percent) +
       theme_bw() + 
-      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+      theme(panel.grid.major = element_blank(), 
+            panel.grid.minor = element_blank()) +
       labs(title="Histogram",y="percent")
   })
-  # Render the csv file in the Raw data tab ---------------------------
-  output$contents <- renderTable({
-    read.csv(file = 'water_potability.csv')
+  # Render a Scatter plot ------------------------------------------------------
+  output$scatterplot <- renderPlot({
+    ggplot(water_potability, 
+      aes(x=!!input$variable_for_x, y=!!input$variable_for_y)) + 
+      geom_point(size = 1, shape = 18) +
+      labs(title = "Scatter plot")
   })
-  # Connect the csv file to the download button -------------------------
+  # Render a Dot plot ----------------------------------------------------------
+  output$dotplot <- renderPlot({
+    ggplot(water_potability, aes(x=Potability, 
+      y=!!input$variable_potability)) +
+      geom_jitter(width = 0.25, ) 
+  })
+  
+  # Render the csv file in the Raw data tab ------------------------------------
+  output$contents <- renderPrint({
+    orig <- options(width = 1000)
+    print(tail(water_potability, input$maxrows), row.names = FALSE)
+    options(orig)
+  })
+  # Connect the csv file to the download button --------------------------------
   output$downloadCsv <- downloadHandler(
     filename = 'water_potability.csv',
     content = function(file) {
